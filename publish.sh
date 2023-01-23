c6e38f7630c3f7b94ef441a83f65dc91447cac6b
Mikael Rinnetmäki <mikael@sensotrend.com>
2023-01-23 22:26:41 +0200

    Start building the publishing pipeline
    Does not work yet.

---
+++ publish.sh
@@ -0,0 +1,1 @@
+java -jar input-cache/publisher.jar -go-publish -source . -web fhir -registry ../ig-registry/fhir-ig-list.json -history ../fhir-ig-history-template -templates output -temp temp-publish/
