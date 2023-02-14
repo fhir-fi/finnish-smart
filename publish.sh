cd28e4d6a07cdfa85c22a0a45f99948b0e2f4aa7
Mikael Rinnetmäki <mikael@sensotrend.com>
2023-02-14 19:29:32 +0200

    Add publication config files

--- publish.sh
+++ publish.sh
@@ -1,1 +1,1 @@
-java -jar input-cache/publisher.jar -go-publish -source . -web fhir -registry ../ig-registry/fhir-ig-list.json -history ../fhir-ig-history-template -templates output -temp temp-publish/
+java -jar input-cache/publisher.jar -go-publish -source . -web https://hl7.fi/fhir -registry ../ig-registry/fhir-ig-list.json -history ../fhir-ig-history-template/ -templates ../ig-template-fhir-fi/ -temp temp
